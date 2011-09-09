216020fdd0c7d9d04f8c006ace50908bc9f8a43a
wl war <war@example.com>
2011-09-09 18:54:29 +0200

    export dates as timestamp into file

--- ref.py
+++ ref.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-import csv, re, sys, os.path
+import csv, re, sys, os.path, time
+from datetime import datetime
 
 limit = None
 if len(sys.argv) == 3:
@@ -25,6 +26,7 @@ ref_ids = set()
 ref_cnt = {}
 diff_cnt = {}
 edges = []
+dates = {}
 
 for row in content:
     count = count + 1
@@ -35,6 +37,10 @@ for row in content:
     # read csv values
     line_num, cabledate, cable, location, classification, referrer, head, body = row
 
+    tdate = datetime.strptime(cabledate.strip(), '%m/%d/%Y %H:%M')
+    timestamp = int(time.mktime(tdate.timetuple()))
+    dates.update({cable:timestamp})
+
     cable_ids.add(cable)
     for r in referrer.split('|'):
         if len(r.strip()) > 0:
@@ -44,6 +50,7 @@ for row in content:
                 ref_cnt[r] = ref_cnt[r] + 1
             else:
                 ref_cnt[r] = 1
+
 
 #    re_referrer = re.search(re_ref, body)
 #    if re_referrer is not None:
@@ -64,6 +71,7 @@ print len(ref_ids)
 print len(ref_cnt)
 diff_ids = ref_ids.difference(cable_ids)
 print len(diff_ids)
+print len(dates)
 
 for d in diff_ids:
     diff_cnt[d] = ref_cnt[d]
@@ -76,6 +84,7 @@ df = file('diff_ids.list','w')
 rcf = file('ref_cnt.list','w')
 dcf = file('diff_cnt.list','w')
 ef = file('edges.list','w')
+datef = file('dates.list','w')
 
 for i in sorted(cable_ids):
     cf.write('%s\n' % i)
@@ -100,4 +109,8 @@ dcf.close()
 for e in sorted(edges):
     ef.write('%s %s\n' % e)
 ef.close()
+
+for k,v in sorted(dates.iteritems()):
+    datef.write('%s %s\n' % (k,v))
+datef.close()
 
