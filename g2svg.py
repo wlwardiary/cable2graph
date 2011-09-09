3b8f97468eb5faf124691d4ceef3dc14b6033636
wl war <war@example.com>
2011-09-09 16:28:37 +0200

    add cmd line options, clean- and speed-up code

--- g2svg.py
+++ g2svg.py
@@ -3,135 +3,206 @@ import igraph, math, csv
 from sys import argv, exit
 from os import listdir, path
 from jinja2 import Template, Environment, FileSystemLoader
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-t", "--tmpl", dest="template",
+    default="svg.tmpl",
+    help="Jinja html/svg template NAME. Default: svg.tmpl", 
+    metavar="NAME")
+
+parser.add_option("-g", "--gml", dest="gmlfile",
+    help="Single source graph in gml format FILE", metavar="FILE")
+
+parser.add_option("-l", "--load-from", dest="gmllist",
+    help="Load all .gml graphs listed in FILE", metavar="FILE")
+
+parser.add_option("-d", "--dest", dest="destdir",
+    default=".",
+    help="Destination DIR. Default current dir:", 
+    metavar="DIR")
+
+parser.add_option("-r", "--reverb", dest="reverb",
+    help="Load sentences from a ReVerb result .tsv FILE", metavar="FILE")
+
+parser.add_option("-u", "--uri", dest="uri",
+    help="Load map of label -> uri from FILE", metavar="FILE")
+(options, args) = parser.parse_args()
+
+graph_files = set()
+if options.gmlfile:
+    graph_files.add(options.gmlfile.strip())
+
+if options.gmllist:
+    gfh = open(options.gmllist)
+    for gf in gfh.readlines():
+        graph_files.add(gf.strip())
+
+if len(graph_files) < 1:
+    print "No source .gml files"
+    exit(1)
 
 CURDIR = path.dirname(path.abspath(__file__))
 env = Environment(loader=FileSystemLoader(CURDIR))
-svg_tmpl = env.get_template('svg.tmpl')
+svg_tmpl = env.get_template(options.template)
 
-g = igraph.load(argv[1])
-
-# load ReVerb sentences result .tsv
+# load optional ReVerb sentences result .tsv
 # http://reverb.cs.washington.edu/
 
-reverb_csv = csv.reader(
-        open(CURDIR + '/middle-east.reverb'), 
-        delimiter='\t', 
-        quotechar = None,
-        lineterminator = '\n')
+if options.reverb:
+    reverb_csv = csv.reader(
+            open(options.reverb), 
+            delimiter='\t', 
+            quotechar = None,
+            lineterminator = '\n')
 
-reverb_map = {}
-for row in reverb_csv:
-    type, reverb_cable_id, num, argument1, relation, argument2, score = row
-    if float(score) > 0.7:
-        if reverb_map.has_key(reverb_cable_id):
-            reverb_map[reverb_cable_id].append({
-                    'a1': argument1, 
-                    'rel': relation,
-                    'a2': argument2,
-                    'score': score
-                    })
-        else:
-            reverb_map[reverb_cable_id] = [{ 
-                    'a1': argument1, 
-                    'rel': relation,
-                    'a2': argument2,
-                    'score': score
-                    }]
+    reverb_map = {}
+    for row in reverb_csv:
+        type, reverb_cable_id, num, argument1, relation, argument2, score = row
+        # TODO have a custom threshold as cmd line parameter
+        if float(score) > 0.7:
+            if reverb_map.has_key(reverb_cable_id):
+                reverb_map[reverb_cable_id].append({
+                        'a1': argument1, 
+                        'rel': relation,
+                        'a2': argument2,
+                        'score': score
+                        })
+            else:
+                reverb_map[reverb_cable_id] = [{ 
+                        'a1': argument1, 
+                        'rel': relation,
+                        'a2': argument2,
+                        'score': score
+                        }]
 
-sentence = []
-for l in g.vs.get_attribute_values('label'):
-    if l in reverb_map.keys():
-        sentence.append(reverb_map[l])
-    else:
-        sentence.append('')
+# load optional uri map
+if options.uri:
+    cmap = {}
+    f = open(options.uri)
+    for l in f.readlines():
+        k,v = l.strip().split()
+        cmap[k.strip()] = v.strip()
 
-g.vs['sentence'] = sentence
+for gml in graph_files:
+    destfile = "%s/%s.html" % (options.destdir, path.basename(gml))
+    if path.exists(destfile):
+        print  "%s exists. skipping" % destfile
+        continue
 
-# add uri to graph
-cmap = {}
-f = open(CURDIR + '/all.map')
-for l in f.readlines():
-    k,v = l.strip().split()
-    cmap[k.strip()] = v.strip()
+    # load graph from file
+    g = igraph.load(gml)
 
-uri = []
+    # apply sentences to graph
+    if options.reverb:
+        sentence = []
+        for l in g.vs.get_attribute_values('label'):
+            if l in reverb_map.keys():
+                sentence.append(reverb_map[l])
+            else:
+                sentence.append('')
 
-for l in g.vs.get_attribute_values('label'):
-    if l in cmap:
-        uri.append('http://wikileaks.org/%s' % cmap[l])
-    else:
-        uri.append('')
+        g.vs['sentence'] = sentence
 
-g.vs['uri'] = uri
+    # appy uris to graph
+    if options.uri:
+        uri = []
+        for l in g.vs.get_attribute_values('label'):
+            if l in cmap:
+                # TODO move into uri file
+                uri.append('http://wikileaks.org/%s' % cmap[l])
+            else:
+                uri.append('')
 
-layout = g.layout('fr')
+        g.vs['uri'] = uri
 
-width = len(g.vs) * 20
-height = len(g.vs) * 20
+    # create x,y positions
+    layout = g.layout('fr')
 
-w = '%d' % width
-h = '%d' % height
+    # calc graph size based on number of vertices
+    width = len(g.vs) * 20
+    height = len(g.vs) * 20
+    
+    if width < 800: 
+        width = 800
+    if height < 600: 
+        height = 600
 
-xw = '%.4f' % (width/2.0)
-xh = '%.4f' % (height/2.0)
+    w = '%d' % width
+    h = '%d' % height
 
-labels = g.vs.get_attribute_values('label')
-colors = g.vs.get_attribute_values('color')
-uris = g.vs.get_attribute_values('uri')
-sentences = g.vs.get_attribute_values('sentence')
-vertex_size = 10
+    xw = '%.4f' % (width/2.0)
+    xh = '%.4f' % (height/2.0)
 
-# from igraph.Graph.write_svg
+    labels = g.vs.get_attribute_values('label')
+    colors = g.vs.get_attribute_values('color')
 
-maxs=[layout[0][dim] for dim in range(2)]
-mins=[layout[0][dim] for dim in range(2)]
-        
-for rowidx in range(1, len(layout)):
-    row = layout[rowidx]
-    for dim in range(0, 2):
-        if maxs[dim]<row[dim]: maxs[dim]=row[dim]
-        if mins[dim]>row[dim]: mins[dim]=row[dim]
-        
-sizes=[width-2*vertex_size, height-2*vertex_size]
-halfsizes=[(maxs[dim]+mins[dim])/2.0 for dim in range(2)]
-ratios=[sizes[dim]/(maxs[dim]-mins[dim]) for dim in range(2)]
-layout=[[(row[0]-halfsizes[0])*ratios[0], \
-         (row[1]-halfsizes[1])*ratios[1]] \
-        for row in layout]
+    if options.uri:
+        uris = g.vs.get_attribute_values('uri')
+    if options.reverb:
+        sentences = g.vs.get_attribute_values('sentence')
 
-edges = []
-vertices = []
+    # from igraph.Graph.write_svg
+    vertex_size = 10
 
-for eidx, edge in enumerate(g.es):
-    vidxs = edge.tuple
-    x1 = layout[vidxs[0]][0]
-    y1 = layout[vidxs[0]][1]
-    x2 = layout[vidxs[1]][0]
-    y2 = layout[vidxs[1]][1]
-    angle = math.atan2(y2-y1, x2-x1)
-    x2 = x2 - vertex_size*math.cos(angle)
-    y2 = y2 - vertex_size*math.sin(angle)
-    edges.append(
-        {'x1': '%.4f' % x1, 
-         'y1': '%.4f' % y1, 
-         'x2': '%.4f' % x2, 
-         'y2': '%.4f' % y2 })
+    maxs=[layout[0][dim] for dim in range(2)]
+    mins=[layout[0][dim] for dim in range(2)]
+            
+    for rowidx in range(1, len(layout)):
+        row = layout[rowidx]
+        for dim in range(0, 2):
+            if maxs[dim]<row[dim]: maxs[dim]=row[dim]
+            if mins[dim]>row[dim]: mins[dim]=row[dim]
+            
+    sizes=[width-2*vertex_size, height-2*vertex_size]
+    halfsizes=[(maxs[dim]+mins[dim])/2.0 for dim in range(2)]
+    ratios=[sizes[dim]/(maxs[dim]-mins[dim]) for dim in range(2)]
+    layout=[[(row[0]-halfsizes[0])*ratios[0], \
+             (row[1]-halfsizes[1])*ratios[1]] \
+            for row in layout]
 
-for vidx in range(g.vcount()):
-    vertices.append( { 
-        'x': '%.4f' % layout[vidx][0], 
-        'y': '%.4f' % layout[vidx][1],
-        'label': str(labels[vidx]),
-        'uri': str(uris[vidx]),
-        'sentences': sentences[vidx],
-        'class': str(colors[vidx]) 
-        } )
+    edges = []
+    vertices = []
 
-print svg_tmpl.render(
-    height = h,
-    width = w,
-    xh = xh,
-    xw = xw,
-    edges = edges, 
-    vertices = vertices)
+    for eidx, edge in enumerate(g.es):
+        vidxs = edge.tuple
+        x1 = layout[vidxs[0]][0]
+        y1 = layout[vidxs[0]][1]
+        x2 = layout[vidxs[1]][0]
+        y2 = layout[vidxs[1]][1]
+        angle = math.atan2(y2-y1, x2-x1)
+        x2 = x2 - vertex_size*math.cos(angle)
+        y2 = y2 - vertex_size*math.sin(angle)
+        edges.append(
+            {'x1': '%.4f' % x1, 
+             'y1': '%.4f' % y1, 
+             'x2': '%.4f' % x2, 
+             'y2': '%.4f' % y2 })
 
+    for vidx in range(g.vcount()):
+        tmpd = {
+            'x': '%.4f' % layout[vidx][0], 
+            'y': '%.4f' % layout[vidx][1],
+            'label': str(labels[vidx]),
+            'class': str(colors[vidx]) 
+        }
+        # optional
+        if options.uri:
+            tmpd.update({'uri': str(uris[vidx])})
+
+        if options.reverb:
+            tmpd.update({'sentences': sentences[vidx]})
+
+        vertices.append(tmpd)
+
+    dfh = open(destfile,'w')
+    dfh.write(svg_tmpl.render(
+        height = h,
+        width = w,
+        xh = xh,
+        xw = xw,
+        edges = edges, 
+        vertices = vertices))
+    print "%s -> %s" % (gml, destfile)
+
+# GPLv3 anonymous
