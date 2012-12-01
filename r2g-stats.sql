b13084a26794e1906ec2228d0d89acf409403eec
wl war <war@example.com>
2012-12-01 19:37:43 -0300

    remove HAVING

--- r2g-stats.sql
+++ r2g-stats.sql
@@ -1,4 +1,4 @@
 -- from whom did the CIA receive cables over the years
-SELECT sndr, COUNT(rcvr) AS c, year(created) AS y 
+SELECT sndr, COUNT(rcvr) AS c, YEAR(created) AS y 
 FROM msg WHERE rcvr = 'RUEAIIA' 
-GROUP BY sndr, y HAVING c > 0 ORDER BY y, c;
+GROUP BY sndr, y ORDER BY y, c;
