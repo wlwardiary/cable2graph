b7f96f62e4540bc06f0f39350b6b2d04c81a7769
wl war <war@example.com>
2012-12-03 18:01:35 -0300

    3 CIA querys

--- r2g-stats-RUEAIIA.sql
+++ r2g-stats-RUEAIIA.sql
@@ -1,9 +1,31 @@
--- from whom did the CIA receive cables over the years
+-- from whom did the CIA receive how many cables?
+SELECT 
+sndr, 
+COUNT(DISTINCT(mrn)) AS c 
+FROM msg 
+WHERE rcvr IN ('RUEAIIA') 
+GROUP BY sndr 
+ORDER BY c;
+
+-- from whom did the CIA receive how many cables over the years?
 SELECT 
 sndr,
 COUNT(DISTINCT(mrn)) AS c, 
 YEAR(created) AS y
 FROM msg 
-WHERE rcvr IN ('RUEAIIA','CIA WASHDC')
+WHERE rcvr IN ('RUEAIIA')
 GROUP BY sndr, y 
 ORDER BY y, c;
+
+-- from whom did the CIA receive more then 30 cables each month?
+SELECT 
+sndr,
+COUNT(DISTINCT(mrn)) AS c, 
+YEAR(created) AS y,
+MONTH(created) AS m
+FROM msg 
+WHERE rcvr IN ('RUEAIIA')
+GROUP BY sndr, y, m
+HAVING c > 30
+ORDER BY y, m, c;
+
